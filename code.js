06d2f96efe10992f166c6da566538aef837901d1
larrysl2 <109638483+larrysl2@users.noreply.github.com>
2022-09-21 20:21:24 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,65 @@
-var p=0;
-var c=0;
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+var p =0;
+var c = 0;
+var t=0;
+var playerSelections = '';
+
+paper.addEventListener("click", returnpaper);
+    function returnpaper() {
+        playerSelections = 'PAPER';
+        var cc = getComputerChoice();
+        var result = playRound(playerSelections,cc);
+        document.querySelector(".youselected").innerHTML = ("You selected: Paper");
+        document.querySelector(".compselected").innerHTML = ("Computer selected: " +cc);
+        document.querySelector(".result").innerHTML = (result);
+        document.querySelector(".pwins").innerHTML = ("Player wins: " +p);
+        document.querySelector(".cwins").innerHTML = ("Computer wins: " +c);
+        document.querySelector(".ties").innerHTML = ("Ties: " + t);
+        if (p==5)
+        {
+            
+            alert("PLAYER WINS")
+            alert("CHOOSE IMAGE FOR NEW GAME");
+            p=0;
+            c=0;
+            t=0;
+        }
+        else if (c==5)
+        {
+            alert("COMPUTER WINS")
+            alert("CHOOSE IMAGE FOR NEW GAME");
+            p=0;
+            c=0;
+            t=0;
+        }
+}
+rock.addEventListener("click", returnrock);
+    function returnrock() {
+        playerSelections = "ROCK";
+        var cc = getComputerChoice();
+        var result = playRound(playerSelections,cc);
+        document.querySelector(".youselected").innerHTML = ("You selected: Rock");
+        document.querySelector(".compselected").innerHTML = ("Computer selected: " +cc);
+        document.querySelector(".result").innerHTML = (result);
+        document.querySelector(".pwins").innerHTML = ("Player wins: " +p);
+        document.querySelector(".cwins").innerHTML = ("Computer wins: " +c);
+        document.querySelector(".ties").innerHTML = ("Ties: " + t);
+}
+scissors.addEventListener("click", returnscissors);
+    function returnscissors() {
+        playerSelections = "SCISSORS";
+        var cc = getComputerChoice();
+        var result = playRound(playerSelections,cc);
+        document.querySelector(".youselected").innerHTML = ("You selected: Scissors");
+        document.querySelector(".compselected").innerHTML = ("Computer selected: " +cc);
+        document.querySelector(".result").innerHTML = (result);
+        document.querySelector(".pwins").innerHTML = ("Player wins: " +p);
+        document.querySelector(".cwins").innerHTML = ("Computer wins: " +c);
+        document.querySelector(".ties").innerHTML = ("Ties: " + t);
+}
+
 function getComputerChoice()
 {
     x=Math.floor(Math.random()*3);
@@ -19,50 +79,42 @@ function getComputerChoice()
             return 'SCISSORS';
         }  
 }
-function playerSelectionf(){
-    let playerSelection = '';
-    var ps=0
-    do
-    {
-        let playerSelectionU = prompt("Select rock, paper or scissors")
-         playerSelection = playerSelectionU.toUpperCase();
-        if((playerSelection == 'ROCK' || playerSelection =='PAPER' || playerSelection == 'SCISSORS'|| playerSelection==null))
-        {
-            ps =1
-            
-        }
-    } while(ps!=1);
-    return playerSelection;
-    
-}
-   
+// function playerSelectionf(){
+//     let playerSelection = '';
+//     var ps=0
+//     do
+//     {
+//         let playerSelectionU = prompt("Select rock, paper or scissors")
+//          playerSelection = playerSelectionU.toUpperCase();
+//         if((playerSelection == 'ROCK' || playerSelection =='PAPER' || playerSelection == 'SCISSORS'|| playerSelection==null))
+//         {
+//             ps =1
+//         }
+//     } while(ps!=1);
+//     return playerSelection;   
+// }
 
 function playRound(playerSelections, computerselection)
 {
-    
-    switch(true )
+    switch(true)
     {
-        
         case playerSelections=== computerselection:
-        console.log(playerSelections);
-        console.log( "Tie!");
-        break;
+        t++;
+        return( "Tie!");
+        
         
         case ((playerSelections === 'ROCK' && computerselection=== 'PAPER') ||(playerSelections === 'SCISSORS' && computerselection=== 'ROCK') ||(playerSelections === 'PAPER' && computerselection==='SCISSORS')):
-            console.log(playerSelections);
         c++;
-        console.log ("You Lose! " + computerselection+ " beats " +playerSelections);
-        break;
+        return ("You Lose! " + computerselection+ " beats " +playerSelections);
+        
        
-        case ((playerSelections === 'ROCK' && computerselection=== 'SCISSORS') ||(playerSelections === 'SCISSORS' && computerselection=== 'PAPER') ||(playerSelections === 'PAPER' && computerselection==='ROCK')):
-            console.log(playerSelections);
+        case ((playerSelections === 'ROCK' && computerselection=== 'SCISSORS') ||(playerSelections === 'SCISSORS' && computerselection=== 'PAPER') ||(playerSelections === 'PAPER' && computerselection==='ROCK')):   
         p++;
-        console.log ( "You Win! " + playerSelections+ " beats " + computerselection);
-        break;
-       
+        return ( "You Win! " + playerSelections+ " beats " + computerselection);
         
     }
 }
+
 
 // function game()
 // {
